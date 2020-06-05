6b91b6e55ea8f31a49bdb38d13745cb41b47d96f
Samarth Srivastava <samarthsrivastava009@gmail.com>
2020-06-05 14:07:29 +0530

    Refractoring date.js

--- date.js
+++ date.js
@@ -1,7 +1,5 @@
 
-module.exports=getDate;
-
-function getDate() {
+module.exports=function() {
   var today = new Date();
   var currentDay = today.getDay();
   var other = today.getFullYear()
@@ -15,3 +13,21 @@ function getDate() {
   day = today.toLocaleDateString("en-US", options);
   return day;
 }
+
+//The above code is simply a refractored version of the code that's mentioned below in the commented form: The refractoring is done by using the declaration of JS function using the expression method whereby which we've managed to reduce the redundancy of the getDate which appears many times therein the the commented code down there...
+// module.exports=getDate;
+//
+// function getDate() {
+//   var today = new Date();
+//   var currentDay = today.getDay();
+//   var other = today.getFullYear()
+//
+//   options = {
+//     weekday: 'long',
+//     year: 'numeric',
+//     month: 'long',
+//     day: 'numeric'
+//   };
+//   day = today.toLocaleDateString("en-US", options);
+//   return day;
+// }
